a1847447849b5ac61355cb0ea0bd84330f0a5ac3
danielrutiloCSUN <daniel.rutilo.473@my.csun.edu>
2024-02-24 16:11:02 -0800

    fixed componentDidUpdate

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -56,8 +56,8 @@ class Graph extends Component<IProps, {}> {
   componentDidUpdate() {
     if (this.table) {
       this.table.update([
-        DataManipulator.generateRow(this.props.data),
-        ] as unknown as TableData);
+            DataManipulator.generateRow(this.props.data),
+        ] as unknown as string);
     }
   }
 }
